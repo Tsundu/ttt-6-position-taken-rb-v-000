fa995f94ac0aac191a33ffc15f5932a0c1005aae
Daniel Osment <osment.tsundu@gmail.com>
2020-01-07 06:49:02 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index.to_i] == ("X" || "O")  #board[index.to_i] == "O"
+  if board[index.to_i] == "X" || board[index.to_i] == "O"
     true
   else
     false
